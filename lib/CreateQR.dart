4cb42bf91ed4c8fd9ebc159df9759dfd7dfb201c
varshilj.patel <mail.varshil@gmail.com>
2023-07-25 00:00:51 +0530

    v0.3

--- lib/CreateQR.dart
+++ lib/CreateQR.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:qr_kode/ui/appbar.dart';
-import 'package:share/share.dart';
 
 class CreateQR extends StatefulWidget {
   final String contentDataBase;
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
-  const CreateQR({super.key, required this.contentDataBase});
+  CreateQR({super.key, required this.contentDataBase});
 
   @override
   State<CreateQR> createState() => _CreateQRState();
@@ -16,7 +16,6 @@ class CreateQR extends StatefulWidget {
 
 class _CreateQRState extends State<CreateQR> {
   late final String _contentData;
-  // ScreenshotController screenshotController = ScreenshotController();
 
   @override
   void initState() {
@@ -75,8 +74,40 @@ class _CreateQRState extends State<CreateQR> {
   }
 
   // Share QR Code
-  void _downloadQrCode() {
+  Future<String?>? _downloadQrCode() async {
+    try {
+      Directory? directory = await getExternalStorageDirectory();
+      String filename = "QrKode-${DateTime.now().millisecondsSinceEpoch}.png".toString();
+      File file = File("${directory?.path}/$filename");
+      await file.writeAsBytes(await _generateQrCodeBytes(_contentData));
+      // print("Saved");
+      widget._scaffoldMessengerKey.currentState?.showSnackBar(
+        SnackBar(content: Text("QR code saved to: ${file.path}")),
+      );
+      print(file);
+      print(file.path);
+      return file.path;
+    } catch (e) {
+      print("e :: $e");
+    }
+    return null;
+  }
 
+  _generateQrCodeBytes(String data) async {
+    try {
+      final qrImage = await QrPainter(
+        data: data,
+        version: QrVersions.auto,
+        color: Colors.black,
+        emptyColor: Colors.white,
+        gapless: true,
+      ).toImageData(300);
+
+      return qrImage?.buffer.asUint8List();
+    } catch (e) {
+      print("Error generating QR code: $e");
+      return [];
+    }
   }
 
   @override
@@ -86,97 +117,107 @@ class _CreateQRState extends State<CreateQR> {
       body: Padding(
         padding: const EdgeInsets.all(24.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
 
-            children: [
-              const Text("Download", style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.w500
-              ),),
-
-              const SizedBox(
-                height: 36.0,
-              ),
-
-              // Qr image
-              _generateQrCode(),
-
-              const SizedBox(
-                height: 36.0,
-              ),
-
-              ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: 225
-                ),
-
-                child: const Text('Scan this QR Code for get your text or link.', textAlign: TextAlign.center, style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w400,
+              children: [
+                const Text("Download", style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w500
                 ),),
-              ),
 
-              const SizedBox(
-                height: 48.0,
-              ),
-
-              ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: 200
+                const SizedBox(
+                  height: 36.0,
                 ),
 
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // Qr image
+                _generateQrCode(),
 
-                  children: [
-                    // Share icon
-                    InkWell(
-                      onTap: () {
-                        _shareQrCode();
-                      },
+                const SizedBox(
+                  height: 36.0,
+                ),
 
-                      customBorder: const RoundedRectangleBorder(
+                ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxWidth: 225
+                  ),
+
+                  child: const Text('Scan this QR Code for get your text or link.', textAlign: TextAlign.center, style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                  ),),
+                ),
+
+                const SizedBox(
+                  height: 48.0,
+                ),
+
+                ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxWidth: 200
+                  ),
+
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                    children: [
+                      // Share icon
+                      InkWell(
+                        onTap: () {
+                          _shareQrCode();
+                        },
+
+                        customBorder: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(1000))
+                        ),
+
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 4.0, color: Colors.white),
+                              borderRadius: const BorderRadius.all(Radius.circular(1000))
+                          ),
+
+                          child: const Padding(
+                            padding: EdgeInsets.all(12.0),
+
+                            child: Icon(Icons.share, color: Colors.white, size: 36.0),
+                          )
+                        ),
+                      ),
+
+                      // Download icon
+                      InkWell(
+                        onTap: () async {
+                          String? filePath = await _downloadQrCode();
+                          SnackBar snackBar = SnackBar(
+                            content: Text("QR code saved to: $filePath"),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        },
+                        
+                        customBorder: const RoundedRectangleBorder(
                           borderRadius: BorderRadius.all(Radius.circular(1000))
-                      ),
-
-                      child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(width: 4.0, color: Colors.white),
-                            borderRadius: const BorderRadius.all(Radius.circular(1000))
                         ),
 
-                        child: const Padding(
-                          padding: EdgeInsets.all(12.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 4.0, color: Colors.white),
+                              borderRadius: const BorderRadius.all(Radius.circular(1000))
+                          ),
 
-                          child: Icon(Icons.share, color: Colors.white, size: 36.0),
-                        )
-                      ),
-                    ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(12.0),
 
-                    // Download icon
-                    InkWell(
-                      onTap: () {
-                        _downloadQrCode();
-                      },
-
-                      child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(width: 4.0, color: Colors.white),
-                            borderRadius: const BorderRadius.all(Radius.circular(1000))
+                            child: Icon(Icons.download_sharp, color: Colors.white, size: 36.0,),
+                          )
                         ),
-
-                        child: const Padding(
-                          padding: EdgeInsets.all(12.0),
-
-                          child: Icon(Icons.download_sharp, color: Colors.white, size: 36.0,),
-                        )
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ),
       ),
